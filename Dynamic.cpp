e97c1bfde029770906c724a90db7e083630c85d6
vrusha10 <110154094+vrusha10@users.noreply.github.com>
2022-10-20 17:11:28 +0530

    Update Dynamic.cpp

--- Dynamic.cpp
+++ Dynamic.cpp
@@ -4,12 +4,12 @@ using namespace std;
 class A
 {
 public:
-     void fun1()
+     virtual void fun1()
     {
         cout<<"Fun1 A"<<endl;
     }
 };
-class B
+class B:public A
 {
 public:
     void fun1()
@@ -19,8 +19,14 @@ public:
 };
 int main()
 {
-    B *b=new B();
-   A *a=reinterpret_cast<A*>(b);
-   a->fun1();
-   b->fun1();
+     A *a=dynamic_cast<A*>(new B);
+     if(a){
+          a->fun1();
+          cout<<"Valid A"<<endl;
+     }
+     B *b=dynamic_cast<B*>(new A);
+     if(b){
+          b->fun1();
+          cout<<"Valid B"<<endl;
+     }
 }
